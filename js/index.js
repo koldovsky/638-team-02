9dfa4850c0151cdd920ceefd20b1bd763d6f1251
KORZHILLIA <kiev_drum2006@ukr.net>
2021-10-21 13:10:33 +0000

    Something

--- js/index.js
+++ js/index.js
@@ -1,2 +1,19 @@
-const sectionSlogan = document.querySelector('.section-slogan');
+const sectionSlogan = document.querySelector('.section-header .section-text .section-slogan');
 sectionSlogan.innerText += '!';
+//setInterval( () => sectionSlogan.hidden = !sectionSlogan.hidden, 500 );
+
+const clockContainer = document.querySelector('.clock');
+setInterval( () => clockContainer.innerText = new Date().toLocaleTimeString(), 1000);
+
+const countDownContainer = document.querySelector('.countdown');
+
+const startTime = new Date();
+
+function showCountDown() {
+    const limitSeconds = 10;
+    const currentTime = new Date();
+    const intervalSeconds = Math.round((currentTime - startTime) / 1000);
+    const leftSeconds = limitSeconds - intervalSeconds;
+    countDownContainer.innerText = leftSeconds;
+}
+setInterval(showCountDown, 1000);
