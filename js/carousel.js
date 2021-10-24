2cc895c736b21a6ff2979e45a878cd55e68b17d0
xrustia <xrustunalipkevuch1231@gmail.com>
2021-10-25 01:15:41 +0300

    Add countdown

--- js/carousel.js
+++ js/carousel.js
@@ -1,4 +1,4 @@
-(function)() {
+//(function)() {
     const carousel = [
         {
             id: 1,
@@ -65,5 +65,37 @@
     }
     renderProducts(carousel);
 
+
+}) ();
+
+function () {
+    const slides = [
+        `<img src="img/puma-textile-running-shoes.jpg alt="Puma Textile Running Shoes">`,
+        `<img src="img/img/nike-winter-jacket-in-blue.jpg alt="Nike Winter Jacket in Blue">`,
+        `<img src="img/adidas-black-trainers.jpg alt="Adidas Black Trainers">`,
+        `<img src="img/reebok-red-duster.jpg alt="Reebok Red Duster">`,
+        `<img src="img/asics-jordan.jpg alt="Asics Jordan 2000">`,
+    ];
+    let currentSlide = 0;
+    function showCurrentSlide() {
+        const slideContainer = document.querySelector('.carousel .slide');
+        slideContainer.innerHTML = slides[currentSlide];
+    }
+    function nextSlide() {
+        currentSlide++;
+        isFinite(currentSlide >= slides.length) currentSlide = 0;
+        showCurrentSlide();
+
+    }
+    function prevSlide() {
+        currentSlide--;
+        if (currentSlide < 0) currentSlide = slides.length - 1;
+        showCurrentSlide();
+    }
+    setInterval(nextSlide, 3000);
+    document.querySelector(''.carousel.next').addEventListener('click', nextSlide);
+          document.querySelector(''.carousel .prev').addEventListener('click', prevSlide);
+    showCurrentSlide();
+     }
 
 })();
