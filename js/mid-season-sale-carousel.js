a6123f9abceaa2e19b50d35bf2b6a9abc6e6356c
KORZHILLIA <kiev_drum2006@ukr.net>
2021-10-25 20:23:51 +0000

    Mid-season sale buttons and events

--- js/mid-season-sale-carousel.js
+++ js/mid-season-sale-carousel.js
@@ -7,7 +7,7 @@
 
     let currentSlide = 0;
     function showCurrentSlide() {
-        const slideContainer = document.querySelector('.catalogue-content .catalogue-images .main-image');
+        const slideContainer = document.querySelector('.catalogue-content .catalogue-images .carousel .main-image');
         slideContainer.innerHTML = slides[currentSlide];
     }
     function nextSlide() {
@@ -15,7 +15,15 @@
         if(currentSlide >= slides.length) currentSlide = 0;
         showCurrentSlide();
     }
-    setInterval(nextSlide, 2000);
+    function prevSlide() {
+        currentSlide--;
+        if(currentSlide < 0) currentSlide = slides.length - 1;
+        showCurrentSlide();
+    }
+
+    setInterval(nextSlide, 5000);
+    document.querySelector('.catalogue-content .catalogue-images .carousel .prev').addEventListener('click', prevSlide);
+    document.querySelector('.catalogue-content .catalogue-images .carousel .next').addEventListener('click', nextSlide);
     showCurrentSlide();
 
 })();
