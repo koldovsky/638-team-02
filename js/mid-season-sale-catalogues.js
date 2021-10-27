65a436431ad54ec0bca0c19f47723aa43d3ae349
KORZHILLIA <kiev_drum2006@ukr.net>
2021-10-27 20:21:27 +0000

    JSON Efforts

--- js/mid-season-sale-catalogues.js
+++ js/mid-season-sale-catalogues.js
@@ -1,56 +1,16 @@
-(function() {
+(async function() {
 
-    const cataloguesTexts = [
-        {
-            id: 1,
-            header: 'ADIDAS CLOUDFOAM TRAINERS',
-            code: '25',
-            price: '60.00',
-            description: 'These color block trainers provide breathability, stability, and durability due to Adidas high-tech mesh. Lightweight EVA foam cushioning makes every your step easier.'
-        },
-        {
-            id: 2,
-            header: 'NIKE CLASSIC POLO T-SHIRT',
-            code: '35',
-            price: '23.00',
-            description: 'This polo T-shirt is for those who love sports casual style and wear polo shirts in everyday life. The slightly fitted silhouette will highlight your workout progress.'
-
-        },
-        {
-            id: 3,
-            header: 'PUMA FITNESS SHOES IN GREY',
-            code: '24',
-            price: '60.00',
-            description: 'These ultra-lightweight mesh sneakers are great for your fitness workout or regular active lifestyle. The EVA midsole is so comfortable you may want to wear these shoes long after your run has ended.'
-
-        },
-        {
-            id: 4,
-            header: 'PUMA ESSENTIAL TANK TOP',
-            code: '39',
-            price: '43.00',
-            description: 'Run in style and confidence in this Puma tank top. High-tech fabric wicks away sweat to keep you in comfort. Classic black color suits everyone.'
-
-        },
-        {
-            id: 5,
-            header: 'REEBOK WORKOUT SHORTS',
-            code: '34',
-            price: '25.00',
-            description: 'These blue shorts are ready for your muscle-burning gym sessions. The lightweight fabric wicks sweat to keep you dry and in comfort.'
-
-        },
-    ];
+    const response = await fetch('mid-season-sale-catalogues.json');
+    const cataloguesTexts = await response.json;
 
     function renderProducts(cataloguesTexts) {
         const cataloguesTextsContainer = document.querySelector('.catalogue-text');
-        for(const cataloguesText of cataloguesTexts) {
-            cataloguesTextsContainer.innerHTML += `
+            cataloguesTextsContainer.innerHTML = `
             <div>
                 <p class="catalogue-presence">In Stock</p>
-                <h3 class="catalogue-header">${cataloguesText.header}</h3>
-                <p class="catalogue-code">Product code ${cataloguesText.code}</p>
-                <p class="catalogue-price">$${cataloguesText.price}</p>
+                <h3 class="catalogue-header">${cataloguesTexts[0].header}</h3>
+                <p class="catalogue-code">Product code ${cataloguesTexts[0].code}</p>
+                <p class="catalogue-price">$${cataloguesTexts[0].price}</p>
             </div>
             <div class="catalogue-reference">
                 <div class="cart-management">
@@ -68,9 +28,8 @@
             </div>
             <div>
                 <h5 class="description-header">Description</h5>
-                <p class="goods-description">${cataloguesText.description}</p>
+                <p class="goods-description">${cataloguesTexts[0].description}</p>
             </div>`
         }
-    }
     renderProducts(cataloguesTexts);
 })();
