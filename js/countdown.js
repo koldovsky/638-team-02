4d16272573236bfce67bfb2ec1a9b8654e8249a7
xrustia <xrustunalipkevuch1231@gmail.com>
2021-10-29 13:54:28 +0300

    Add countdown

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,5 @@
 (function () {
-    const countDownContainer = document.querySelector('.number');
+    const countDownContainer = document.querySelector('.number-day');
     const startTime = new Date();
     function showCountDown() {
         const limitSecs = 405;
@@ -11,3 +11,44 @@
     }
     setInterval(showCountDown, 1000);
 })();
+(function () {
+    const countDownContainer = document.querySelector('.number-hours');
+    const startTime = new Date();
+    function showCountDown() {
+        const limitSecs = 21;
+        const currentTime = new Date();
+        const intervalSecs = Math.round((currentTime - startTime) / 1000);
+        const leftSecs = limitSecs - intervalSecs;
+        countDownContainer.innerText = leftSecs;
+
+    }
+    setInterval(showCountDown, 1000);
+})();
+(function () {
+    const countDownContainer = document.querySelector('.number-minutes');
+    const startTime = new Date();
+    function showCountDown() {
+        const limitSecs = 29;
+        const currentTime = new Date();
+        const intervalSecs = Math.round((currentTime - startTime) / 1000);
+        const leftSecs = limitSecs - intervalSecs;
+        countDownContainer.innerText = leftSecs;
+
+    }
+    setInterval(showCountDown, 1000);
+})();
+
+(function () {
+    const countDownContainer = document.querySelector('.number-seconds');
+    const startTime = new Date();
+    function showCountDown() {
+        const limitSecs = 29;
+        const currentTime = new Date();
+        const intervalSecs = Math.round((currentTime - startTime) / 1000);
+        const leftSecs = limitSecs - intervalSecs;
+        countDownContainer.innerText = leftSecs;
+
+    }
+    setInterval(showCountDown, 1000);
+})();
+
