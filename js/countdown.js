2b2c71e599601673e84c0c89a3dc25c33a327720
xrustia <xrustunalipkevuch1231@gmail.com>
2021-10-29 13:36:18 +0300

    Add countdown

--- js/countdown.js
+++ js/countdown.js
@@ -1,11 +1,13 @@
-const countDownContainer = document.querySelector('.countdown');
-const startTime = new Date();
-function showCountDown() {
-    const limitSecs = 10;
-    const currentTime = new Date();
-    const intervalSecs = Math.round((currentTime - startTime) / 1000);
-    const left = limitSecs - intervalSecs;
-    countDownContainer.innerText = leftSecs;
+(function () {
+    const countDownContainer = document.querySelector('.number');
+    const startTime = new Date();
+    function showCountDown() {
+        const limitSecs = 405;
+        const currentTime = new Date();
+        const intervalSecs = Math.round((currentTime - startTime) / 1000);
+        const leftSecs = limitSecs - intervalSecs;
+        countDownContainer.innerText = leftSecs;
 
-}
-setInterval(showCountDown, 1000);
+    }
+    setInterval(showCountDown, 1000);
+})();
