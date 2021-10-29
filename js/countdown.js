fd5af012e942c7f1672f3a35cde3911605613bd3
xrustia <xrustunalipkevuch1231@gmail.com>
2021-10-29 14:50:29 +0300

    Add countdown

--- js/countdown.js
+++ js/countdown.js
@@ -2,40 +2,40 @@
     const countDownContainer = document.querySelector('.number-day');
     const startTime = new Date();
     function showCountDown() {
-        const limitSecs = 405;
+        const limitDay = 405;
         const currentTime = new Date();
-        const intervalSecs = Math.round((currentTime - startTime) / 1000);
-        const leftSecs = limitSecs - intervalSecs;
-        countDownContainer.innerText = leftSecs;
+        const intervalDay = Math.round((currentTime - startTime) / 100000);
+        const leftDay = limitDay - intervalDay;
+        countDownContainer.innerText = leftDay;
 
     }
-    setInterval(showCountDown, 1000);
+    setInterval(showCountDown, 100000);
 })();
 (function () {
     const countDownContainer = document.querySelector('.number-hours');
     const startTime = new Date();
     function showCountDown() {
-        const limitSecs = 21;
+        const limitHour = 21;
         const currentTime = new Date();
-        const intervalSecs = Math.round((currentTime - startTime) / 1000);
-        const leftSecs = limitSecs - intervalSecs;
-        countDownContainer.innerText = leftSecs;
+        const intervalHour = Math.round((currentTime - startTime) / 10000);
+        const leftHour = limitHour - intervalHour;
+        countDownContainer.innerText = leftHour;
 
     }
-    setInterval(showCountDown, 1000);
+    setInterval(showCountDown, 10000);
 })();
 (function () {
     const countDownContainer = document.querySelector('.number-minutes');
     const startTime = new Date();
     function showCountDown() {
-        const limitSecs = 29;
+        const limitMinute = 57;
         const currentTime = new Date();
-        const intervalSecs = Math.round((currentTime - startTime) / 1000);
-        const leftSecs = limitSecs - intervalSecs;
-        countDownContainer.innerText = leftSecs;
+        const intervalMinute = Math.round((currentTime - startTime) / 10000);
+        const leftMinute = limitMinute - intervalMinute;
+        countDownContainer.innerText = leftMinute;
 
     }
-    setInterval(showCountDown, 1000);
+    setInterval(showCountDown, 10000);
 })();
 
 (function () {
